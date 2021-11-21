8bcbdfece8285bf72d34ddd627f56ee700b69f46
raman karki <ramankarki40@gmail.com>
2021-11-21 19:48:29 +0545

    wrap component with Link component

--- src/Components/VolunteerCard/VolunteerCard.jsx
+++ src/Components/VolunteerCard/VolunteerCard.jsx
@@ -1,17 +1,27 @@
 import { Button } from '@mui/material';
+import { Link } from 'react-location';
 import React from 'react';
 import './style.scss';
 
 export default function VolunteerCard(props) {
+  const onBtnClick = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <div className="volunteerCard__container">
-      <img src={props.photo} alt="donor" />
-      <div className="volunteerCard__container__name">
-        {props.first_name} {props.last_name}
+    <Link
+      to={'/volunteer-profile/' + props._id}
+      style={{ textDecoration: 'none', color: 'initial' }}
+    >
+      <div className="volunteerCard__container">
+        <img src={props.photo} alt="donor" />
+        <div className="volunteerCard__container__name">
+          {props.first_name} {props.last_name}
+        </div>
+        <div className="volunteerCard__container__email">{props.email} </div>
+        <p>{props.bio}</p>
+        <Button onClick={onBtnClick}>Donate</Button>
       </div>
-      <div className="volunteerCard__container__email">{props.email} </div>
-      <p>{props.bio}</p>
-      <Button>Donate</Button>
-    </div>
+    </Link>
   );
 }
