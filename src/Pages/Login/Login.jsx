6bb7e3bb9e706c3872c8511de17450b17ae2b239
pawan3s <pawan29s123@gmail.com>
2021-11-21 22:56:30 +0545

    login and signup integration

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.scss";
 
 import Logo from "../../Assets/logo.png";
@@ -6,96 +6,179 @@ import Logo from "../../Assets/logo.png";
 import { Link } from "react-location";
 import { Button } from "@mui/material";
 import Footer from "../../Components/Footer/Footer";
+import axios from "axios";
 
 export default function Login() {
   const [isActiveMenu, setIsActiveMenu] = React.useState(false);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [successful, setSuccessful] = useState(false);
+  const [sending, setSending] = useState(false);
+  const [error, setError] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    setSending(true);
+    setSuccessful(false);
+
+    axios({
+      method: "POST",
+      url: "https://api.hminepal.org/api/v1/users/login",
+      data: {
+        email: email,
+        password: password,
+      },
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then(function (response) {
+        //handle success
+        alert("loggged In");
+        setSending(false);
+        setSuccessful(true);
+      })
+      .catch(function (response) {
+        //handle error
+        setError(response.message);
+        setSending(false);
+      });
+  };
+
   return (
-    <div className="login__container">
+    <div className='login__container'>
       {/* @sectoin => topbar */}
-      <div className="login__container__topbar">
+      <div className='login__container__topbar'>
         <img
-          className="login__container__logo"
+          className='login__container__logo'
           src={Logo}
-          alt="haminepal logo"
+          alt='haminepal logo'
         />
 
         <button onClick={() => setIsActiveMenu(true)}>
-          <i className="ri-menu-line"></i>
+          <i className='ri-menu-line'></i>
         </button>
       </div>
 
       {/* @section => hidden menu */}
       <div
-        className="login__container__landing__hiddenMenu"
+        className='login__container__landing__hiddenMenu'
         style={{
           display: isActiveMenu ? "flex" : "none",
         }}
       >
-        <div className="login__container__landing__hiddenMenu__topbar">
+        <div className='login__container__landing__hiddenMenu__topbar'>
           <img
-            className="login__container__landing__topbar__logo"
+            className='login__container__landing__topbar__logo'
             src={Logo}
-            alt="haminepal logo"
+            alt='haminepal logo'
           />
 
           <button onClick={() => setIsActiveMenu(false)}>
-            <i className="ri-close-line"></i>
+            <i className='ri-close-line'></i>
           </button>
         </div>
-        <ul className="login__container__landing__hiddenMenu__items left">
+        <ul className='login__container__landing__hiddenMenu__items left'>
           <li>
-            <Link to="/">Home</Link>
+            <Link to='/'>Home</Link>
           </li>
           <li>
-            <Link to="/">News</Link>
+            <Link to='/'>News</Link>
           </li>
           <li>
-            <Link to="/">Act of Kindness</Link>
+            <Link to='/'>Act of Kindness</Link>
           </li>
           <li>
-            <Link to="/">Civil Rights Movements</Link>
+            <Link to='/'>Civil Rights Movements</Link>
           </li>
           <li>
-            <Link to="/">Contact Us</Link>
+            <Link to='/'>Contact Us</Link>
           </li>
-          <div className="divider"></div>
+          <div className='divider'></div>
           <li>
-            <Link to="/">Login/</Link> <Link to="/">login</Link>
+            <Link to='/'>Login/</Link> <Link to='/'>login</Link>
           </li>
         </ul>
-        <ul className="login__container__landing__hiddenMenu__items right">
+        <ul className='login__container__landing__hiddenMenu__items right'>
           <li>
-            <Link to="/">About Us</Link>
+            <Link to='/'>About Us</Link>
           </li>
           <li>
-            <Link to="/">Cause</Link>
+            <Link to='/'>Cause</Link>
           </li>
           <li>
-            <Link to="/">Events</Link>
+            <Link to='/'>Events</Link>
           </li>
           <li>
-            <Link to="/">Transparency</Link>
+            <Link to='/'>Transparency</Link>
           </li>
           <li>
-            <Link to="/">Volunteers</Link>
+            <Link to='/'>Volunteers</Link>
           </li>
         </ul>
       </div>
 
       {/* @section => form */}
-      <div className="login__container__form">
+      <div className='login__container__form'>
         {/* @section => form container */}
-        <div className="login__container__form__inputs">
-          <div className="login__container__form__inputs__input left">
-          <h1>Login</h1>
-        <div className="divider"></div>
-            <input className="full" type="email" placeholder="Email Address" />
-            <input className="full" type="password" placeholder="Password" />
-            <Button>Submit</Button>
-          </div>
+        <div className='login__container__form__inputs'>
+          {!successful && (
+            <div className='login__container__form__inputs__input left'>
+              <h1>Login</h1>
+              <div className='divider'></div>
+              <input
+                className='full'
+                type='email'
+                placeholder='Email Address'
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+              <input
+                className='full'
+                type='password'
+                placeholder='Password'
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
+              <Button onClick={handleLogin}>
+                {sending ? "Sending..." : "Login"}
+              </Button>
+              {error && (
+                <div className='form-group'>
+                  <div
+                    className='alert alert-danger'
+                    role='alert'
+                    style={{ color: "red", marginTop: "20px" }}
+                  >
+                    {error}
+                  </div>
+                </div>
+              )}
+              {successful && (
+                <div className='form-group'>
+                  <div
+                    className='alert alert-success'
+                    role='alert'
+                    style={{ color: "green" }}
+                  >
+                    <span>Successfully loggedIn</span>
+                  </div>
+                </div>
+              )}
+            </div>
+          )}
 
-          <div className="login__container__form__inputs__input right">
-            <img src="https://images.unsplash.com/photo-1509822929063-6b6cfc9b42f2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1770&q=80" alt="banner" />
+          <div className='login__container__form__inputs__input right'>
+            <img
+              src='https://images.unsplash.com/photo-1509822929063-6b6cfc9b42f2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1770&q=80'
+              alt='banner'
+            />
           </div>
         </div>
       </div>
--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.scss";
 
 import Logo from "../../Assets/logo.png";
@@ -6,120 +6,222 @@ import Logo from "../../Assets/logo.png";
 import { Link } from "react-location";
 import { Button } from "@mui/material";
 import Footer from "../../Components/Footer/Footer";
+import axios from "axios";
 
 export default function SignUp() {
   const [isActiveMenu, setIsActiveMenu] = React.useState(false);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confrimPassword, setConfrimPassword] = useState("");
+
+  const [successful, setSuccessful] = useState(false);
+  const [sending, setSending] = useState(false);
+  const [error, setError] = useState("");
+
+  const register = (e) => {
+    e.preventDefault();
+    setSending(true);
+    setSuccessful(false);
+
+    // const formData = new FormData();
+    // formData.append("firstname", firstName);
+    // formData.append("lastname", lastName);
+    // formData.append("email", email);
+    // formData.append("password", password);
+
+    axios({
+      method: "POST",
+      url: "https://api.hminepal.org/api/v1/users/signup",
+      data: {
+        firstname: firstName,
+        lastname: lastName,
+        email: email,
+        password: password,
+      },
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then(function (response) {
+        //handle success
+        alert("Registered successfully");
+        setSending(false);
+        setSuccessful(true);
+      })
+      .catch(function (response) {
+        //handle error
+        setError(response.message);
+        setSending(false);
+      });
+  };
 
   return (
-    <div className="signup__container">
+    <div className='signup__container'>
       {/* @sectoin => topbar */}
-      <div className="signup__container__topbar">
+      <div className='signup__container__topbar'>
         <img
-          className="signup__container__logo"
+          className='signup__container__logo'
           src={Logo}
-          alt="haminepal logo"
+          alt='haminepal logo'
         />
 
         <button onClick={() => setIsActiveMenu(true)}>
-          <i className="ri-menu-line"></i>
+          <i className='ri-menu-line'></i>
         </button>
       </div>
 
       {/* @section => hidden menu */}
       <div
-        className="signup__container__landing__hiddenMenu"
+        className='signup__container__landing__hiddenMenu'
         style={{
           display: isActiveMenu ? "flex" : "none",
         }}
       >
-        <div className="signup__container__landing__hiddenMenu__topbar">
+        <div className='signup__container__landing__hiddenMenu__topbar'>
           <img
-            className="signup__container__landing__topbar__logo"
+            className='signup__container__landing__topbar__logo'
             src={Logo}
-            alt="haminepal logo"
+            alt='haminepal logo'
           />
 
           <button onClick={() => setIsActiveMenu(false)}>
-            <i className="ri-close-line"></i>
+            <i className='ri-close-line'></i>
           </button>
         </div>
-        <ul className="signup__container__landing__hiddenMenu__items left">
+        <ul className='signup__container__landing__hiddenMenu__items left'>
           <li>
-            <Link to="/">Home</Link>
+            <Link to='/'>Home</Link>
           </li>
           <li>
-            <Link to="/">News</Link>
+            <Link to='/'>News</Link>
           </li>
           <li>
-            <Link to="/">Act of Kindness</Link>
+            <Link to='/'>Act of Kindness</Link>
           </li>
           <li>
-            <Link to="/">Civil Rights Movements</Link>
+            <Link to='/'>Civil Rights Movements</Link>
           </li>
           <li>
-            <Link to="/">Contact Us</Link>
+            <Link to='/'>Contact Us</Link>
           </li>
-          <div className="divider"></div>
+          <div className='divider'></div>
           <li>
-            <Link to="/">Login/</Link> <Link to="/">Signup</Link>
+            <Link to='/'>Login/</Link> <Link to='/'>Signup</Link>
           </li>
         </ul>
-        <ul className="signup__container__landing__hiddenMenu__items right">
+        <ul className='signup__container__landing__hiddenMenu__items right'>
           <li>
-            <Link to="/">About Us</Link>
+            <Link to='/'>About Us</Link>
           </li>
           <li>
-            <Link to="/">Cause</Link>
+            <Link to='/'>Cause</Link>
           </li>
           <li>
-            <Link to="/">Events</Link>
+            <Link to='/'>Events</Link>
           </li>
           <li>
-            <Link to="/">Transparency</Link>
+            <Link to='/'>Transparency</Link>
           </li>
           <li>
-            <Link to="/">Volunteers</Link>
+            <Link to='/'>Volunteers</Link>
           </li>
         </ul>
       </div>
 
       {/* @section => form */}
-      <div className="signup__container__form">
+      <div className='signup__container__form'>
         <h1>Signup</h1>
-        <div className="divider"></div>
+        <div className='divider'></div>
 
         {/* @section => form container */}
-        <div className="signup__container__form__inputs">
-          <div className="signup__container__form__inputs__input left">
-            <div>
-              <input type="text" placeholder="First Name" />
-              <input type="text" placeholder="Last Name" />
+        <div className='signup__container__form__inputs'>
+          {!successful && (
+            <div className='signup__container__form__inputs__input left'>
+              <div>
+                <input
+                  type='text'
+                  placeholder='First Name'
+                  value={firstName}
+                  onChange={(e) => {
+                    setFirstName(e.target.value);
+                  }}
+                />
+                <input
+                  type='text'
+                  placeholder='Last Name'
+                  value={lastName}
+                  onChange={(e) => {
+                    setLastName(e.target.value);
+                  }}
+                />
+              </div>
+
+              <div>
+                <input
+                  className='full'
+                  type='email'
+                  placeholder='Email Address'
+                  value={email}
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
+                />
+              </div>
+
+              <div>
+                <input
+                  className='full'
+                  type='password'
+                  placeholder='Password'
+                  value={password}
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                />
+                <input
+                  className='full'
+                  type='password'
+                  placeholder='Confirm Password'
+                  value={confrimPassword}
+                  onChange={(e) => {
+                    setConfrimPassword(e.target.value);
+                  }}
+                />
+              </div>
+              <Button onClick={register}>
+                {sending ? "Sending..." : "Register"}
+              </Button>
+              {error && (
+                <div className='form-group'>
+                  <div
+                    className='alert alert-danger'
+                    role='alert'
+                    style={{ color: "red", marginTop: "20px" }}
+                  >
+                    {error}
+                  </div>
+                </div>
+              )}
+              {successful && (
+                <div className='form-group'>
+                  <div
+                    className='alert alert-success'
+                    role='alert'
+                    style={{ color: "green" }}
+                  >
+                    <span>Successfully resgitered</span>
+                  </div>
+                </div>
+              )}
             </div>
+          )}
 
-            <div>
-              <input
-                className="full"
-                type="email"
-                placeholder="Email Address"
-              />
-            </div>
-
-            <div>
-              <input className="full" type="password" placeholder="Password" />
-              <input
-                className="full"
-                type="password"
-                placeholder="Confirm Password"
-              />
-            </div>
-
-            <Button>Submit</Button>
-          </div>
-
-          <div className="signup__container__form__inputs__input right">
+          <div className='signup__container__form__inputs__input right'>
             <img
-              src="https://images.unsplash.com/photo-1588702547919-26089e690ecc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1770&q=80"
-              alt="banner"
+              src='https://images.unsplash.com/photo-1588702547919-26089e690ecc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1770&q=80'
+              alt='banner'
             />
           </div>
         </div>
