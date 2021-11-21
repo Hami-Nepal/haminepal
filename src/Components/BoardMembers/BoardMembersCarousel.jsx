8a952fb24ce3a9c4f60987dd5c98e62d7327c5dc
SudeepMI <sudeepm0599@gmail.com>
2021-11-21 17:58:27 +0545

    board members on home page

--- src/Components/BoardMembers/BoardMembersCarousel.jsx
+++ src/Components/BoardMembers/BoardMembersCarousel.jsx
@@ -1,65 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.scss";
 
 import Carousel from "react-elastic-carousel";
 import { Link } from "react-location";
 
 export default function BoardMembersCarousel() {
-  const members = [
-    {
-      name: "Board Member",
-      position: "President",
-      message:
-        "I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.",
-      memberImage:
-        "https://avatars.githubusercontent.com/u/93444253?s=400&u=389a238cf991d86adcc03166270d30241e94a95b&v=4",
-      socialLinks: {
-        instagram: "https://www.instagram.com/deekshyaaaaa/",
-        facebook: "https://www.facebook.com/deekshyaaaaa",
-        twitter: "https://twitter.com/deekshyaaaaa",
-        linkedin: "https://www.linkedin.com/in/deekshyaaaaa/",
-      },
-    },
-    {
-      name: "Board Member",
-      position: "President",
-      message:
-        "I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.",
-      memberImage:
-        "https://avatars.githubusercontent.com/u/93444253?s=400&u=389a238cf991d86adcc03166270d30241e94a95b&v=4",
-      socialLinks: {
-        instagram: "https://www.instagram.com/deekshyaaaaa/",
-        facebook: "https://www.facebook.com/deekshyaaaaa",
-        twitter: "https://twitter.com/deekshyaaaaa",
-        linkedin: "https://www.linkedin.com/in/deekshyaaaaa/",
-      },
-    },
-    {
-      name: "Board Member",
-      position: "President",
-      message:
-        "I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.",
-      memberImage:
-        "https://avatars.githubusercontent.com/u/93444253?s=400&u=389a238cf991d86adcc03166270d30241e94a95b&v=4",
-      socialLinks: {
-        instagram: "https://www.instagram.com/deekshyaaaaa/",
-        facebook: "https://www.facebook.com/deekshyaaaaa",
-        twitter: "https://twitter.com/deekshyaaaaa",
-        linkedin: "https://www.linkedin.com/in/deekshyaaaaa/",
-      },
-    },
-  ];
+  const [Members, setMembers] = useState([])
+  useEffect(() => {
+    async function fetchData() {
+      await fetch("https://api.haminepal.org/api/v1/boardmembers")
+      .then(response => response.json())
+      .then(data => setMembers(data))
+    }
+    fetchData();
+  },[])
   return (
     <div className="boardMembersCarousel__container">
       <Carousel>
-        {members.map((member, index) => (
+        {Members && Members.map((member, index) => (
           <div className="boardMembersCarousel__container__item" key={index}>
-            <img src={member.memberImage} alt="board member" />
+            <img src={member.photo} alt="board member" />
             <div className="boardMembersCarousel__container__memberName">
               {member.name}
             </div>
             <div className="boardMembersCarousel__container__position">
-              {member.position}
+              {member.designation}
             </div>
             <div className="boardMembersCarousel__container__message">
               "{member.message}"
@@ -67,25 +32,25 @@ export default function BoardMembersCarousel() {
             <div className="boardMembersCarousel__container__socialLinks">
               <ul>
                 <li>
-                  <Link to={member.socialLinks.instagram} target='_blank'>
+                  <Link to={member.instaLink || ''} target='_blank'>
                     {" "}
                     <i className="ri-instagram-line"></i>
                   </Link>
                 </li>
                 <li>
-                  <Link to={member.socialLinks.facebook}>
+                  <Link to={member.facebookLink || ''}>
                     {" "}
                     <i className="ri-facebook-circle-line"></i>
                   </Link>
                 </li>
                 <li>
-                  <Link to={member.socialLinks.twitter}>
+                  <Link to={member.twitterLink || ''}>
                     {" "}
                     <i className="ri-twitter-line"></i>
                   </Link>
                 </li>
                 <li>
-                  <Link to={member.socialLinks.linkedin}>
+                  <Link to={member.linkedLink || ''}>
                     {" "}
                     <i className="ri-linkedin-box-line"></i>
                   </Link>
