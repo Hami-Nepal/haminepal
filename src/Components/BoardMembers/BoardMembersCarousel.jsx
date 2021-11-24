d9ecd19722b73f8338df78796e9f6c0f20a14ab7
raman karki <ramankarki40@gmail.com>
2021-11-24 16:53:18 +0545

    decrease fontsize and fix social links

--- src/Components/BoardMembers/BoardMembersCarousel.jsx
+++ src/Components/BoardMembers/BoardMembersCarousel.jsx
@@ -1,64 +1,81 @@
-import React, { useState, useEffect } from "react";
-import "./style.scss";
+import React, { useState, useEffect } from 'react';
+import './style.scss';
 
-import Carousel from "react-elastic-carousel";
-import { Link } from "react-location";
+import Carousel from 'react-elastic-carousel';
+import baseURL from '../../api/baseURL';
 
 export default function BoardMembersCarousel() {
-  const [Members, setMembers] = useState([])
+  const [Members, setMembers] = useState([]);
   useEffect(() => {
     async function fetchData() {
-      await fetch("https://api.haminepal.org/api/v1/boardmembers")
-      .then(response => response.json())
-      .then(data => setMembers(data.data))
+      await fetch(baseURL + '/boardmembers')
+        .then((response) => response.json())
+        .then((data) => setMembers(data.data));
     }
     fetchData();
-  },[])
+  }, []);
   return (
     <div className="boardMembersCarousel__container">
       <Carousel>
-        {Members && Members.map((member, index) => (
-          <div className="boardMembersCarousel__container__item" key={index}>
-            <img src={member.photo} alt="board member" />
-            <div className="boardMembersCarousel__container__memberName">
-              {member.name}
+        {Members &&
+          Members.map((member, index) => (
+            <div className="boardMembersCarousel__container__item" key={index}>
+              <img src={member.photo} alt="board member" />
+              <div className="boardMembersCarousel__container__memberName">
+                {member.name}
+              </div>
+              <div className="boardMembersCarousel__container__position">
+                {member.designation}
+              </div>
+              <div className="boardMembersCarousel__container__message">
+                "{member.message}"
+              </div>
+              <div className="boardMembersCarousel__container__socialLinks">
+                <ul>
+                  <li>
+                    <a
+                      href={member.instaLink || ''}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      {' '}
+                      <i className="ri-instagram-line"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href={member.facebookLink || ''}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      {' '}
+                      <i className="ri-facebook-circle-line"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href={member.twitterLink || ''}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      {' '}
+                      <i className="ri-twitter-line"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href={member.linkedLink || ''}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      {' '}
+                      <i className="ri-linkedin-box-line"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </div>
-            <div className="boardMembersCarousel__container__position">
-              {member.designation}
-            </div>
-            <div className="boardMembersCarousel__container__message">
-              "{member.message}"
-            </div>
-            <div className="boardMembersCarousel__container__socialLinks">
-              <ul>
-                <li>
-                  <Link to={member.instaLink || ''} target='_blank'>
-                    {" "}
-                    <i className="ri-instagram-line"></i>
-                  </Link>
-                </li>
-                <li>
-                  <Link to={member.facebookLink || ''}>
-                    {" "}
-                    <i className="ri-facebook-circle-line"></i>
-                  </Link>
-                </li>
-                <li>
-                  <Link to={member.twitterLink || ''}>
-                    {" "}
-                    <i className="ri-twitter-line"></i>
-                  </Link>
-                </li>
-                <li>
-                  <Link to={member.linkedLink || ''}>
-                    {" "}
-                    <i className="ri-linkedin-box-line"></i>
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        ))}
+          ))}
       </Carousel>
     </div>
   );
