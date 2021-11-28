0d696527f61d21439c8cbee3d90d27c54b8b9274
raman karki <ramankarki40@gmail.com>
2021-11-28 15:24:24 +0545

    add condition for dev & prod api url

--- src/api/baseURL.js
+++ src/api/baseURL.js
@@ -1,2 +1,6 @@
-const baseURL = "https://api.haminepal.org/api/v1"
-export default baseURL
+let baseURL = 'http://localhost:5000/api/v1';
+
+if (process.env.NODE_ENV === 'production') {
+  baseURL = 'https://api.haminepal.org/api/v1';
+}
+export default baseURL;
