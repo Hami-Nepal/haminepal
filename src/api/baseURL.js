6c4d31f62800abf6d088662ce6cef2e29d872dbd
raman karki <ramankarki40@gmail.com>
2021-12-02 20:30:01 +0545

    please don't change this

--- src/api/baseURL.js
+++ src/api/baseURL.js
@@ -1,7 +1,8 @@
 let baseURL = 'https://api.haminepal.org/api/v1';
 
+console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
-  baseURL = 'https://api.haminepal.org/api/v1';
+  baseURL = 'http://localhost:5000/api/v1';
 }
 
 export default baseURL;
