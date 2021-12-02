ece3468321d5b1de28b773d3be8183306b108943
kishor <kishorbhandari608@gmail.com>
2021-12-03 00:40:14 +0530

    news-section-responsive

--- src/Pages/News/News.jsx
+++ src/Pages/News/News.jsx
@@ -13,21 +13,21 @@ export default function News() {
   const [isActiveMenu, setIsActiveMenu] = React.useState(false)
 
   return (
-    <div className="news">
+    <>
       <Nav />
-      {/* @section=>Landing */}
-      <div className="news__container_landing ">
-        <h1>News</h1>
-        <p>Hami Nepal News</p>
+      <div className="news">
+        {/* @section=>Landing */}
+        <div className="news__container_landing ">
+          <h1>News</h1>
+          <p>Hami Nepal News</p>
+        </div>
+        <h5 style={{ textAlign: "center", margin: "50px 0px 50px 0px" }}>
+          Get all of our latest news, blogs and analysis, updated daily.
+        </h5>
       </div>
-      <h5 style={{ textAlign: "center", margin: "50px 0px 50px 0px" }}>
-        Get all of our latest news, blogs and analysis, updated daily.
-      </h5>
-
       {/* @section=>news cards */}
       <NewsTabs />
-
       <Footer />
-    </div>
+    </>
   )
 }
