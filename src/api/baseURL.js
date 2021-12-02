86e42217e387930692b6d8ec8093efabf8d4ea20
raman karki <ramankarki40@gmail.com>
2021-12-02 20:30:23 +0545

    remove console

--- src/api/baseURL.js
+++ src/api/baseURL.js
@@ -1,6 +1,5 @@
 let baseURL = 'https://api.haminepal.org/api/v1';
 
-console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
   baseURL = 'http://localhost:5000/api/v1';
 }
