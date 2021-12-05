205aa9e33cb4cf56c3840119aed79a124a995e2b
ramankarki <ramankarki40@gmail.com>
2021-12-05 16:12:52 +0545

    small styling fixings

--- src/Components/KindnessCard/KindnessCardPage.jsx
+++ src/Components/KindnessCard/KindnessCardPage.jsx
@@ -1,40 +1,40 @@
-import React from "react"
-import "./style.scss"
+import React from 'react';
+import './style.scss';
 
-import { Link } from "react-location"
+import { Link } from 'react-location';
 
-import Pagination from "@mui/material/Pagination"
-import Stack from "@mui/material/Stack"
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
 
-import axios from "axios"
+import axios from 'axios';
 // import baseURL from "../../api/baseURL"
 
 export default function KindnessCardPage(props) {
-  const [posts, setPosts] = React.useState([])
-  const [loading, setLoading] = React.useState(false)
-  const [currentPage, setCurrentPage] = React.useState(1)
-  const [totalData, setTotalData] = React.useState(0)
-  const [count, setCount] = React.useState(0)
+  const [posts, setPosts] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+  const [currentPage, setCurrentPage] = React.useState(1);
+  const [totalData, setTotalData] = React.useState(0);
+  const [count, setCount] = React.useState(0);
 
   const paginate = (e, number) => {
-    setCurrentPage(number)
-  }
+    setCurrentPage(number);
+  };
 
   React.useEffect(() => {
     const fetchPosts = async () => {
-      setLoading(true)
+      setLoading(true);
       const res = await axios.get(
         `https://api.haminepal.org/api/v1/kindness?page=${currentPage}`
-      )
-      setPosts(res.data.data)
-      setTotalData(res.data.total_data)
-      setCount(res.data.count)
-      setLoading(false)
-    }
-    fetchPosts()
-  }, [currentPage])
+      );
+      setPosts(res.data.data);
+      setTotalData(res.data.total_data);
+      setCount(res.data.count);
+      setLoading(false);
+    };
+    fetchPosts();
+  }, [currentPage]);
 
-  const page = Math.ceil(totalData / 10)
+  const page = Math.ceil(totalData / 10);
 
   if (loading) {
     return (
@@ -43,31 +43,27 @@ export default function KindnessCardPage(props) {
           <span class="visually-hidden">Loading...</span>
         </div>
       </div>
-    )
+    );
   }
 
   return (
     <div className="kindness-header">
       <h3>Act of kindness</h3>
-      <div className="kindness-neck">
-        <div class="kindness-hero">
-          {posts.map((data) => (
-            <div className="kindnessCard__container" key={data._id}>
-              <Link to={"/kindness-focused/" + data._id}>
-                <img alt={data._id} src={data.photos[0]} />
-                <div className="kindnessCard__container__title">
-                  {data.title}
-                </div>
-                <div className="kindnessCard__container__description">
-                  {data.summary}
-                </div>
-              </Link>
-            </div>
-          ))}
-        </div>
+      <div class="kindness-hero">
+        {posts.map((data) => (
+          <div className="kindnessCard__container" key={data._id}>
+            <Link to={'/kindness-focused/' + data._id}>
+              <img alt={data._id} src={data.photos[0]} />
+              <div className="kindnessCard__container__title">{data.title}</div>
+              <div className="kindnessCard__container__description">
+                {data.summary}
+              </div>
+            </Link>
+          </div>
+        ))}
       </div>
 
-      <div style={{ margin: "20px" }}></div>
+      <div style={{ margin: '20px' }}></div>
       <Stack justifyContent="center" alignItems="center" spacing={2}>
         <Pagination
           count={page}
@@ -79,5 +75,5 @@ export default function KindnessCardPage(props) {
         />
       </Stack>
     </div>
-  )
+  );
 }
