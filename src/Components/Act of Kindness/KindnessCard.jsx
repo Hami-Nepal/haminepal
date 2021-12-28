68b7d96e0ec0c93880cd7653111ceb169c9110b5
pawan3s <pawan29s123@gmail.com>
2021-12-28 13:21:03 +0545

    asas

--- src/Components/Act of Kindness/KindnessCard.jsx
+++ src/Components/Act of Kindness/KindnessCard.jsx
@@ -5,23 +5,14 @@ import { Link } from "react-location";
 
 export default function KindnessCard(props) {
   return (
-    <div className='kindness__container1'>
-      <div className='kindness__neck1'>
-        <div class='kindness__hero1'>
-          <div className='kindnessCard__container1' key={props._id}>
-            <Link to={"/kindness__focused/" + props._id}>
-              <img alt='kindness' src={props.photos[0]} />
-              <div className='kindnessCard__container__title1'>
-                {props.title}
-              </div>
-              <div className='kindnessCard__container__summary1'>
-                {props.summary}
-              </div>{" "}
-              <span>See More</span>
-            </Link>
-          </div>
+    <div className='kindnessCard__container'>
+      <Link to={"/kindness-focused/" + props._id}>
+        <img alt='example' src={props.photos[0]} />
+        <div className='kindnessCard__container__title'>{props.title}</div>
+        <div className='kindnessCard__container__description'>
+          {props.summary}
         </div>
-      </div>
+      </Link>
     </div>
   );
 }
